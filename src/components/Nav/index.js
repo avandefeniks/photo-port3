d36ab4bf3b317c80cb982f4646cdc364a95acb0d
avandefeniks <angel.vandefeniks@gmail.com>
2022-12-01 12:49:18 -0800

    added gallery component. updated the nav component to use props and useEffect

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,21 +1,19 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
 
-function Nav() {
+function Nav(props) {
 
-    const categories = [
-        {
-          name: "commercial",
-          description:
-            "Photos of grocery stores, food trucks, and other commercial projects",
-        },
-        { name: "portraits", description: "Portraits of people in my life" },
-        { name: "food", description: "Delicious delicacies" },
-        {
-          name: "landscape",
-          description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-        },
-      ];
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory
+    } = props
+
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
 
       function categorySelected(name) {
         console.log(`${name} clicked`);
@@ -23,16 +21,19 @@ function Nav() {
 
     return (
 
-        <header>
+        <header className='flex-row px-1'>
             <h2>
-                <a href='/'>
-                    <span role='img' aria-label='camera'>📸</span> Oh Snap!
+                <a data-testid='link' href='/'>
+                    <span role='img' aria-label='camera'>
+                        {' '}
+                        📸</span>{' '
+                        } Oh Snap!
                 </a>
             </h2>
             <nav>
                 <ul className='flex-row'>
                     <li className='mx-2'>
-                        <a href='#about'>
+                        <a data-testid='link' href='#about'>
                             About me
                         </a>
                     </li>
@@ -41,11 +42,14 @@ function Nav() {
                     </li>
                     {categories.map((category) => (
                         <li
-                        className='mx-1'
-                        key={category.name}
-                        >
-                            <span onClick={() => categorySelected(category.name)} >
-                                {category.name}
+                        className={`mx-1 ${
+                            currentCategory.name === category.name && 'navActive'
+                            }`} key={category.name}>
+                            <span 
+                            onClick={() => {setCurrentCategory(category)
+                            }} 
+                            >
+                                {capitalizeFirstLetter(category.name)}
                             </span>
                         </li>
                     ))}
